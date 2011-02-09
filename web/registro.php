ba2c9824074e681d4a4336fa96415c3388884c0a
blanco <hamtonio@gmail.com>
2011-02-09 11:19:14 +0100

    primer commit registro

---
+++ web/registro.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Formulario de contacto en AJAX</title>
+<link rel="stylesheet" type="text/css" href="estilos.css">
+<script src="jquery.js" type="text/javascript"></script>
+<script src="jquery.validate.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="funciones.js"></script>
+
+
+
+</head>
+
+<body>
+
+<div id="formContenedor">
+	<form id="formulario" autocomplete="off" method="post" action="">
+		<div id="transparencia">
+			<div id="transparenciaMensaje">Caja transparente</div>
+		</div>
+		<table>
+			<tbody>
+				<tr>
+					<td class="label"><label id="lusuario" for="inputUser">Nombre:</label></td>
+					<td class="campo"><input class="inputNormal" type="text" id="inputUser" ></td>
+					<td class="ayuda"><img src="ayuda.gif" alt="Ayuda" onmouseover="muestraAyuda(event, 'Nombre Usuario')"></td>
+				</tr>
+				<tr>
+					<td class="label">Correo electrónico:</td>
+					<td class="campo"><input class="inputNormal" type="text" id="inputCorreo"></td>
+					<td class="ayuda"><img src="ayuda.gif" alt="Ayuda" onmouseover="muestraAyuda(event, 'Correo')"></td>
+				</tr>
+				<tr>
+					<td class="label">Contraseña:</td>
+					<td class="campo"><input class="inputNormal" type="text" id="inputPass"></td>
+					<td class="ayuda"><img src="ayuda.gif" alt="Ayuda" onmouseover="muestraAyuda(event, 'Contraseña')"></td>
+				</tr>
+				<tr>
+					<td class="label">Repetir contraseña:</td>
+					<td class="campo"><input class="inputNormal" type="text" id="inputRpass"></td>
+					<td class="ayuda"><img src="ayuda.gif" alt="Ayuda" onmouseover="muestraAyuda(event, 'Repetir Contraseña')"></td>
+				</tr>
+			</tbody>
+		</table>
+		<br>
+		<div>
+			<button id="botonEnviar" onClick="validaForm()" type="button">Enviar</button>
+			<button type="reset">Borrar</button>
+		</div>
+	</form>
+</div>
+
+<!-- Capa para mostrar los mensajes de ayuda al presionar los iconos correspondientes -->
+<div id="mensajesAyuda">
+	<div id="ayudaTitulo"></div>
+	<div id="ayudaTexto"></div>
+</div>
+</body>
+</html>
