24547d3c598e7a69dcb31e0eab7710f876ccea43
ruben dario <rubentxu@rubentxu-Kubuntu.(none)>
2011-02-09 19:48:02 +0100

    ok

--- ptuit/php/index.php
+++ ptuit/php/index.php
@@ -18,7 +18,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
 <?php
-require_once ( '../php/controladorFrontal.php');
+require_once ( $_SERVER['DOCUMENT_ROOT'].'/php/controladorFrontal.php');
 controladorFrontal::arranca();
 ?>
  
