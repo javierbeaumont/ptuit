7c5f77d6374fda78c36847f33818117a02aee74c
JoseManuelDominguez <josedominguez1199@yahoo.es>
2011-02-09 12:12:35 +0100

    login.php terminado. Añadido controlador de páginas de usuario UserLoginCtrl.php

--- web/login.php
+++ web/login.php
@@ -1,4 +1,6 @@
-<?php echo '<?xml version=\"1.0\" encoding=\"UTF-8\" ?>'."\n"; ?>
+<?php echo '<?xml version=\"1.0\" encoding=\"UTF-8\" ?>'."\n"; 
+/*Author: JoseManuelDominguez
+  Date:   08.02.2011 */?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtmll/DTD/xhtmll.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 <head>
@@ -13,6 +15,8 @@
     </fieldset>
   </form>
   <?php
+    //Queda por cargar aquí el controlador para comprobar que el nick/pass exista en la BBDD
+    include '../php/UserLoginCtrl.php'; //Carga el controlador de carga de página de usuario
     if(isset($error)) {
       if($error) {
         echo '<div id="error">';
@@ -20,7 +24,10 @@
         echo '</div>';
       }
       else {
-        header('Location: http://www.example.com/');
+        //Si no hay errores, se carga la página de usuario con el formato 'user'+nick.php
+        $userPage = new UserLoginCtrl($_POST['nick']);
+         header('Location: '.$userPage->getUserPage());
+
       }
     }
   ?>
@@ -29,8 +36,7 @@
 date_default_timezone_set('UTC');
 
 // Imprime algo como: Monday 8th of August 2005 03:12:46 PM
-echo '© Ptuit '.date('Y');
-
+echo 'Copyright Ptuit '.date('Y');
 ?>
 </body>
 </html>
