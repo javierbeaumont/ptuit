8a9ea8ef3fb87a1191832c0a3f8c0ddef21148b2
Gemma Agramonte <gemma_agra@yahoo.es>
2011-02-09 18:02:52 +0100

    funciones terminadas

--- bd.php
+++ bd.php
@@ -2,7 +2,7 @@
 
 private class bd{
 
-  private function conexionBd(){
+  private function conexionBd(){   //crea la conexion con la BD
     try {
         $db = new PDO('mysql:host=localhost', 'root', '');
         $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, TRUE);
@@ -10,14 +10,14 @@ private class bd{
     } catch (PDOException $e) {
         cabecera('Error grave');
         print "<p>Error: No puede conectarse con la BD.</p>\n";
-     //  print "<p>Error: " . $e->getMessage() . "</p>\n";
-        pie();
+        //  print "<p>Error: " . $e->getMessage() . "</p>\n";
+        //pie();
         exit();
     }
     $db = conexionBd();
   }
 
-  private function desconexionBd() {
+  private function desconexionBd() {   //desconecta la BD por seguridad
    $db = NULL; 
   }
 
@@ -26,81 +26,93 @@ private class bd{
 
 
 
-  private function recogerDatos{
+  private function recogerPrepararDatos($tabla,$array){  
+  //con prepare recojo datos en vble array y le quito comas/comillas... etc.
+
+    for ($i=0; $i<count($array); $i++)  
+    {       
+      $bd->bindParam($i,$array[$i]);
+      $arrayPrepared[$i]=$array[$i].', ';      
+      print ($arrayPrepared[$i]);
+    }  
+    $arrayPrepared[$i]=rtrim($arrayPrepared[$i],", ");
+    return $arrayPrepared;
+
+   /* $stmt = $db->prepare("INSERT INTO REGISTRY (name, value) VALUES (:name, :value)");
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':value', $value);*/
 
     //$permiso=array($_GET['name'],$_GET['password']);
-
-
-  /*  if USUARIO.name=$_GET['name'] and USUARIO.password=$_GET['password'] then
+    /*  if USUARIO.name=$_GET['name'] and USUARIO.password=$_GET['password'] then
     
-
     $tabla=array("nombreTabla", "b", "c");
     array_push($tabla, "d", "e", "f"); 
-
-    foreach ($tabla as $actual)
-    echo $actual . "<br>"; */
-
-   /* $stmt = $bd->prepare("CAST SomeStoredProcedure(?, ?)");
-      $stmt ->execute(array($someInParameter1, $someInParameter2)); */
-  }
-
-  private function recogerConsulta($campos){
-
-  //prepare recojo datos en vble string y separado con comas.
-
-  /*$stmt = $bd->prepare("SELECT * FROM ? where name = ?");
+      
+   /*
+    $stmt = $bd->prepare("SELECT * FROM ? where name = ?");
     if ($stmt->execute(array($_GET['name']))) {
     while ($row = $stmt->fetch()) {
       print_r($row);
     }
-
-    $stmt = $bd->prepare("SELECT * FROM ? where name LIKE ?");
-    $stmt->execute(array("%$_GET[name]%"));*/
-
+*/
 }
   
-  private function select($campos){
+  private function select($tabla){   
 
     $db = conexionBd();
-
-    $consulta = "SELECT $campos FROM $tabla"; 
+    $arrayPrepared=recogerPrepararDatos();
+    $consulta = $db->prepare("SELECT $arrayPrepared FROM $tabla");
+   
     $result = $db->query($consulta);
     if (!$result) { print "<p>Error en la consulta.</p>\n";} 
     else {
-        print "<p>Consulta ejecutada.</p>\n";
-	foreach ($result as $valor) {
-	    print "<p>$valor[nombre] $valor[password]</p>\n";  //para comprobar, poner x campos
+          print "<p>Consulta ejecutada.</p>\n";
+          foreach ($result as $valor) {
+	    print "<p>$valor</p>\n";  //para comprobar, poner x campos
 	}
     }
     $db = NULL;
     /*$stmt->execute();    
     print "procedure returned $return_value\n";*/
-}
- 
+} 
 
-  private function insert($campos){
+  private function insert($tabla){     
 
     $db = conectaDb();
-    $consulta = "INSERT INTO $tabla values (NULL, $campos)";
+    $arrayPrepared=recogerPrepararDatos();
+
+    $consulta = $db->prepare("INSERT INTO $tabla VALUES ($arrayPrepared)");
+ 
     if ($db->query($consulta)) { print "<p>Registro creado correctamente.</p>\n"; }
     else { print "<p>Error al crear el registro.<p>"; }
     $db = NULL;
-
   }
 
-  private function delete($campos){
+  private function delete($tabla){   
     $db = conectaDb();
-    $consulta = "DELETE FROM $tabla WHERE id=$indice";
+    $arrayPrepared=recogerPrepararDatos();
+
+    $consulta = "DELETE FROM $tabla WHERE id=$arrayPrepared[0]";
     if ($db->query($consulta)) { print "<p>Registro borrado correctamente.</p>\n";} 
     else { print "<p>Error al borrar el registro.</p>\n"; }
     $db = NULL;
 }
 
-  private function update($campos){
+  private function update($tabla, $campos){    // $campos es un array con los nombres de los camposTabla
 
     $db = conectaDb();
-    $consulta = "UPDATE $tabla SET nombre=$nombre, apellidos=$apellidos, correo=$correo 
-                 WHERE id=$modificar";  //pasar $campos
+    $arrayPrepared=recogerPrepararDatos();
+
+    for ($i=0; $i<count($arrayPrepared); $i++)  
+    {
+      if ($arrayPrepared[1]==null)  $i++;      
+      $arrayPrepared[$i]= $arrayPrepared[$i]." ".$campos[$i]."=".$arrayPrepared[$i].", ";      
+      print ($arrayPrepared[$i]);
+    }      
+    $arrayPrepared[$i]=rtrim($arrayPrepared[$i],", ");
+
+    $consulta = $db->prepare("UPDATE $tabla SET $arrayPrepared WHERE id=$arrayPrepared[0]");
+     
     if ($db->query($consulta)) { print "<p>Registro modificado correctamente.</p>\n"; } 
     else {  print "<p>Error al modificar el registro.</p>\n"; }
     $db = NULL;
