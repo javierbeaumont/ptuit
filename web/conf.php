41ac05fe3f6b3b340265944854d29f2fe61b3ff6
Estitxu <estitxuainaga@gmail.com>
2011-02-09 10:34:39 +0100

    subido archivo de conf de bbdd

---
+++ web/conf.php
@@ -0,0 +1,16 @@
+<?php
+
+// ** MySQL settings ** //
+/** The name of the database */
+define('DB_NAME', 'ptuit');
+
+/** MySQL database username */
+define('DB_USER', 'ptuit');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'ptuit');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+?>
