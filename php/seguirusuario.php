74e4f95992274aa96f304ea6ced47b2b6035b1b6
Igarki Rico <stalingrado43@gmail.com>
2011-02-10 17:51:45 +0100

    Added connect to DDBB and showFollowings functions

--- php/seguirusuario.php
+++ php/seguirusuario.php
@@ -1,8 +1,7 @@
  <?php
-
 # ptuit
 #
-# Copyright © 2011 Mikel Martinez e Igarki Rico <ptuit@ptuit.org>
+# Copyright © 2011 Mikel Martinez eta Igarki Rico <ptuit@ptuit.org>
 #
 # This file is part of ptuit.
 #
@@ -18,21 +17,41 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with ptuit. If not, see <http://www.gnu.org/licenses/>.
-############################################################################
-	
-	include '../web/conf.php';
-        include 'clases/sesiones.php';        
 
-           function seleccionmensajes(){  
-                Sesiones::crearSesion(5); //Codigo de prueba
-         	$login=$_SESSION['idUser'];
-           	$sql="SELECT usuario.nombre, mensaje.mensaje FROM seguirusuario JOIN mensaje JOIN usuario WHERE seguirusuario.idseguidor='$login' && seguirusuario.idseguido=mensaje.usuario && seguirusuario.idseguido=usuario.id ORDER BY mensaje.fecha;";
-                $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD); 
-                mysql_select_db(DB_NAME, $link); 
-                $result = mysql_query($sql, $link); 
-                while ($row = mysql_fetch_row($result)){            
+
+
+	include '../web/conf.php';
+        include 'sesiones.php';        
+
+//* Funcion conectar a la base de datos. Devuelve la conexion.           
+	    function connect(){
+	      Sesiones::crearSesion(5); //Codigo de prueba
+	      $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD); 
+              mysql_select_db(DB_NAME, $link); 
+	      return $link;
+            }
+
+//* Funcion mostrar los ultimos mensajes de los usuarios que estas siguiendo (Followings).
+	    function showMessages(){ 
+	       $link=connect();    
+               $login=$_SESSION['idUser'];
+               $sql="SELECT usuario.nombre, mensaje.mensaje FROM seguirusuario JOIN mensaje JOIN usuario WHERE seguirusuario.idseguidor='$login' && seguirusuario.idseguido=mensaje.usuario && seguirusuario.idseguido=usuario.id ORDER BY mensaje.fecha;";
+               $result = mysql_query($sql, $link); 
+               while ($row = mysql_fetch_row($result)){            
                   echo "$row[0] &nbsp;"; 
                   echo "$row[1] <br/>";            
-                }  
-           }
+               }  
+             }
+
+//* Funcion mostrar la lista de usuarios que estas siguiendo (Followings).
+	   function showFollowing(){
+		$link=connect();
+		$login=$_SESSION['idUser'];
+		$sql="SELECT usuario.nombre FROM usuario JOIN seguirusuario WHERE seguirusuario.idseguidor='$login' && seguirusuario.idseguido=usuario.id;";
+		$result = mysql_query($sql, $link); 
+                while ($row = mysql_fetch_row($result)){            
+                  echo "$row[0] <br/>";                         
+               }  
+             }
+
 ?> 
