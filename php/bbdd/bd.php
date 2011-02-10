1723eccdd27802c2032dc47124866fbf2a680ecc
Gemma Agramonte <gemma_agra@yahoo.es>
2011-02-10 16:43:21 +0100

    probando

--- php/bbdd/bd.php
+++ php/bbdd/bd.php
@@ -1,27 +1,44 @@
 <?php
 include "../../web/conf.php";
 
+# ptuit
+#
+# Copyright © 2011 Gemma Agramonte <mail>
+#
+# This file is part of ptuit.
+#
+# ptuit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# ptuit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ptuit. If not, see <http://www.gnu.org/licenses/>.
+
+
 class bd{
 
-  protected $bd;
+  protected $db;
 
   protected function conexionBd(){   //crea la conexion con la BD
     try {
-        $db = new PDO('mysql:host='.DB_HOST, DB_USER, DB_PASSWORD);
-        $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, TRUE);
-        return($db);
-    } catch (PDOException $e) {
-        print($e);
-        print "<p>Error: No puede conectarse con la BD.</p>\n";
-        //  print "<p>Error: " . $e->getMessage() . "</p>\n";
-        //pie();
+        $this->db = new PDO('mysql:host='.DB_HOST, DB_USER, DB_PASSWORD);
+        $this->db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, TRUE);
+        return($this->db);
+    } catch (PDOException $e) {       
+        print "<p>Error: No puede conectarse con la BD." .$e->getMessage(). "</p>\n";
         exit();
     }
-    $db = conexionBd();
+    $this->db=conexionBd();
   }
 
   protected function desconexionBd() {   //desconecta la BD por seguridad
-   $db = NULL; 
+   $this->db = NULL; 
   }
 
   protected function recogerPrepararDatosSelect($tabla,$array){   //con prepare recojo datos en vble array y le quito comas/comillas... etc.
@@ -29,7 +46,8 @@ class bd{
     for ($i=0; $i<count($array); $i++)       
     {       
       if ($array[1]==null and $tabla=="usuario") $i++; 
-      $bd->bindParam($i,$array[$i]);
+      $a= $this->db;
+      $a->bindParam($i,$array[$i]);
       $arrayPrepared[$i]=$array[$i].', ';      
       print ($arrayPrepared[$i]);
     }  
@@ -41,7 +59,8 @@ class bd{
  
     for ($i=0; $i<count($array); $i++)       
     { if ($array[1]==null and $tabla=="usuario") $i++;       
-      $bd->bindParam($i,$array[$i]);
+      $a= $this->db;
+      $a->bindParam($i,$array[$i]);
       $arrayPrepared[$i]= $arrayPrepared[$i]." ".$campos[$i]."=".$arrayPrepared[$i]." and ";       
       print ($arrayPrepared[$i]);
     }  
@@ -54,13 +73,14 @@ class bd{
     //preparo los valores introducidos sin comas...
     for ($i=0; $i<count($array); $i++)       
     { if ($array[1]==null and $tabla=="usuario") $i++;       
-      $bd->bindParam($i,$array[$i]);          
+      $a= $this->db;
+      $a->bindParam($i,$array[$i]);          
       print ($array[$i]);
     }     
 
     // añado los = y las comas para formar la estructura
     foreach ($array as $campo=>$valor)
-    {    $arrayPrepared= $arrayPrepared." ".$campo."=".$valor.", ";   }
+    {    $arrayPrepared= $arrayPrepared." ".$campo."=".$valor.", ";   }  //quitar =
     $arrayPrepared=rtrim($arrayPrepared,", ");
     return $arrayPrepared; 
 }
@@ -69,13 +89,13 @@ class bd{
   
   protected function select($tabla,$arraySelect,$arrayWhere){   
 
-    $db = conexionBd();
+    $this->db = conexionBd();
+    
+    $consulta = $this->db->prepare("SELECT $arrayPreparedSelect FROM $tabla WHERE $arrayPreparedWhere");
     $arrayPreparedSelect=$this->recogerPrepararDatosSelect($tabla,$arraySelect);  
     $arrayPreparedWhere=$this->recogerPrepararDatosWhere($tabla,$arrayWhere); 
    
-    $consulta = $db->prepare("SELECT $arrayPreparedSelect FROM $tabla WHERE $arrayPreparedWhere");
-   
-    $result = $db->query($consulta);
+    $result = $this->db->query($consulta);
     if (!$result) { 
         print "<p>Error en la consulta.</p>\n"; 
         return false;
@@ -87,19 +107,19 @@ class bd{
 	  return true;
 	}
     }
-    $db = NULL;    
-   
+    $this->db = NULL;       
   } 
+
   protected function update($tabla,$arraySet,$arrayWhere){    
 
-    $db = conectaDb();
+    $this->db = conexionBd();
     //$arrayPreparedSelect=recogerPrepararDatosSelect();
-    $arrayPreparedWhere=$this->recogerPrepararDatosWhere($tabla,$arrayWhere); 
-    $arrayPreparedSet=$this->recogerPrepararDatos_campoValor($tabla,$arraySet);   
-
-    $consulta = $db->prepare("UPDATE $tabla SET $arrayPreparedSet WHERE $arrayPreparedWhere");
      
-    if ($db->query($consulta)) {
+    $consulta = $this->db->prepare("UPDATE $tabla SET $arrayPreparedSet WHERE $arrayPreparedWhere");
+    $arrayPreparedWhere=$this->recogerPrepararDatosWhere($tabla,$arrayWhere); 
+    $arrayPreparedSet=$this->recogerPrepararDatos_campoValor($tabla,$arraySet); 
+     
+    if ($this->db->query($consulta)) {
        print "<p>Registro modificado correctamente.</p>\n"; 
        return true;
     } 
@@ -107,18 +127,18 @@ class bd{
        print "<p>Error al modificar el registro.</p>\n"; 
        return false;
     }
-    $db = NULL;
+    $this->db = NULL;
    }
 
    protected function insert($tabla,$array){     
 
-    $db = conectaDb();
+    $this->db=conexionBd();
     //$arrayPrepared=recogerPrepararDatosSelect($tabla,$array);
-    $arrayPreparedInsert=$this->recogerPrepararDatos_campoValor($tabla,$array);   
    
-    $consulta = $db->prepare("INSERT INTO $tabla VALUES ($arrayPreparedInsert)");
+    $consulta=$this->db->prepare("INSERT INTO $tabla VALUES ($arrayPreparedInsert)");
+    $arrayPreparedInsert=$this->recogerPrepararDatos_campoValor($tabla,$array);   
  
-    if ($db->query($consulta)) { 
+    if ($this->db->query($consulta)) { 
        print "<p>Registro creado correctamente.</p>\n"; 
        return true;
     }
@@ -126,22 +146,23 @@ class bd{
        print "<p>Error al crear el registro.<p>"; 
        return false;
     }
-    $db = NULL;
+    $this->db = NULL;
    }
 
   protected function delete($tabla,$arrayWhere){   
-    $db = conectaDb();
+    $this->db=conexionBd();
+  
+    $consulta="DELETE FROM $tabla WHERE $arrayPreparedWhere";
     $arrayPreparedWhere=$this->recogerPrepararDatosWhere($tabla,$arrayWhere); 
 
-    $consulta = "DELETE FROM $tabla WHERE $arrayPreparedWhere";
-    if ($db->query($consulta)) { 
+    if ($this->db->query($consulta)) { 
         print "<p>Registro borrado correctamente.</p>\n";
         return true;
     } 
     else { 
         print "<p>Error al borrar el registro.</p>\n"; 
         return false;}
-    $db = NULL;
+    $this->db = NULL;
   }
 }
 ?>
